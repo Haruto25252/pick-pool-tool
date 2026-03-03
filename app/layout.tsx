76760b55d9950d6d28b43a26f417843cb99bd107
Haruto25252 <haruhiro.u@icloud.com>
2026-03-04 07:19:44 +0900

    スコア詳細モーダル追加・BAN効果表示

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
             </svg>
           </a>
         </div>
-        <p className="text-xs">VERSION v1.41</p>
+        <p className="text-xs">VERSION v1.45</p>
         <p className="text-xs">© 2026 Produced by Harun</p>
       </footer>
       </body>
