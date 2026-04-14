fc0d0737661c2800b06f4c04ec45956517524eb8
Haruto25252 <haruhiro.u@icloud.com>
2026-04-14 14:39:15 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,12 +51,6 @@ export default function RootLayout({
       {children}
         <footer className="bg-gray-900 pb-6 flex flex-col items-center gap-2 text-gray-600">
           <div className="flex gap-4">
-          <a href="https://x.com/Harun0502" target="_blank" rel="noopener noreferrer"
-            className="hover:text-gray-400 transition-colors">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
-              <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-4.714-6.231-5.401 6.231H2.748l7.73-8.835L1.254 2.25H8.08l4.259 5.631zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
-            </svg>
-          </a>
           <a href="https://github.com/Haruto25252/pick-pool-tool" target="_blank" rel="noopener noreferrer"
             className="hover:text-gray-400 transition-colors">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
@@ -64,8 +58,8 @@ export default function RootLayout({
             </svg>
           </a>
         </div>
-        <p className="text-xs">VERSION v2.61</p>
-        <p className="text-xs">© 2026 Produced by Harun</p>
+        <p className="text-xs">VERSION v2.62</p>
+        <p className="text-xs">© 2026 Produced by Haruto25252</p>
       </footer>
       <SpeedInsights />
       </Providers>
